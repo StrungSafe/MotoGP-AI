a9a47c813b7bfcde1e0e9b6fa8fbb983f29f53f8
Miguel Molina Jr <admin@miguelmolinajr.com>
2023-05-17 22:37:04 -0400

    hello world

---
+++ MotoGP-AI/Scraper/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Scraper
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            //pdf readers to try
+            //pdfPig
+            //iTextSharp
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
