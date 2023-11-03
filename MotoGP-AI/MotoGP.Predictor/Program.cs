416e4425b9c7f2f47dcd910b55dc0d1ce49697a1
mmolina <mmolina@sscinc.com>
2023-11-03 13:33:54 -0400

    Add prediction project

---
+++ MotoGP-AI/MotoGP.Predictor/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Data;
+
+namespace MotoGP.Predictor
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+            builder.Services
+                   .AddHttpClient(builder.Configuration["MotoGP:Name"],
+                       client =>
+                       {
+                           client.BaseAddress = new Uri(builder.Configuration["MotoGP:BaseAddress"], UriKind.Absolute);
+                       });
+
+            IHost host = builder.Build();
+
+            Console.WriteLine("Finished w/ no errors...");
+            Console.WriteLine("Press <enter> to close");
+            Console.ReadLine();
+        }
+    }
+}
